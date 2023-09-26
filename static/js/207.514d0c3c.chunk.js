22361e667f1d8a603c93a65df3245734b0e9d831
irailcha <115711713+irailcha@users.noreply.github.com>
2023-09-26 13:55:29 +0000

    Deploying to gh-pages from @ irailcha/goit-react-hw-05-movies@129eeccdc95f7a5646de46f21f7f081c915dc20e 🚀

---
+++ static/js/207.514d0c3c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[207],{207:function(e,r,s){s.r(r);var n=s(420),t=s(791),c=s(87),i=s(689),l=s(184);r.default=function(){return(0,l.jsxs)(l.Fragment,{children:[(0,l.jsxs)(n.L,{children:[(0,l.jsx)(c.rU,{to:"/",children:"Home"}),(0,l.jsx)(c.rU,{to:"/movies",children:"Movies"})]}),(0,l.jsx)(t.Suspense,{fallback:(0,l.jsx)("div",{children:"Loading..."}),children:(0,l.jsx)(i.j3,{})})]})}}}]);
+//# sourceMappingURL=207.514d0c3c.chunk.js.map
